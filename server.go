a8d98084e6b0a84fd6f5359413d4a95237996ffd
oscarvazquez <oscar.vazquez2012@gmail.com>
2016-10-23 01:51:40 -0700

    clean up

--- server.go
+++ server.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/oscarvazquez/eleven/config"
-	// "github.com/oscarvazquez/eleven/db"	
 	"log"
 )
 const portnumber = ":8080"
 
 func main(){
 	fmt.Printf("Starting server on port %s\n", portnumber)
-	// db.CreateSession()
 	router := config.SetRoutes()
 	log.Fatal(http.ListenAndServe(portnumber, router))	
 }
